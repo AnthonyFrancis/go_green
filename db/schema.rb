58a6e233e0e8a5f133e9113924376608d80c38c5
Anthony Francis <tonytrack@gmail.com>
2021-12-08 19:36:12 +0000

    Removed Active Record

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_113650) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "albums", force: :cascade do |t|
-    t.string "title"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_albums_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "albums", "users"
 end
